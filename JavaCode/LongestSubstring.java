8c57dbe1ff9244ad1228ecc8968a5fa4ea77f39b
马传宸 <machuanchen13@gmail.com>
2018-02-27 01:59:31 +0800

    Update LongestSubstring.java

--- JavaCode/LongestSubstring.java
+++ JavaCode/LongestSubstring.java
@@ -15,22 +15,20 @@ public class LongestSubstring {
 	public static int LongestSubstringNum(String s) {
 		Set<Character> set =new HashSet<>();
 		int max = 0;
-        int i = 0;
-        int j = 0;
-        int n = s.length();
-		
-        while(i<n && j<n) {
-        		if(!set.contains(s.charAt(j))) {
-        			set.add(s.charAt(j));
-                    j++;
-                    max = Math.max(max, j-i);
-        		}
-        		else {
-                    set.remove(s.charAt(i));
-                    i++;
-        		}
-        }
-        
-        return max;
+		int i = 0;
+		int j = 0;
+		int n = s.length();
+		while(i<n && j<n) {
+			if(!set.contains(s.charAt(j))) {
+				set.add(s.charAt(j));
+			    	j++;
+			    	max = Math.max(max, j-i);
+			}
+			else {
+				set.remove(s.charAt(i));
+			    	i++;
+			}
+		}
+		return max;
     }
 }
